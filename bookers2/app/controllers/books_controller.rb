459aca0bd4f17a4fc7f862f71632165192fa1ef5
fmy1995 <fmy.kymz444@gmail.com>
2020-12-04 06:26:53 +0000

    12/4

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -4,17 +4,23 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def index
+    @books = Book.page(params[:page]).reverse_order
+    @book = Book.new
+  end
+  
   def create
     @book = Book.new(book_params)
+    @books = Book.page(params[:page]).reverse_order
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book)
+    else
+    render 'index'
+    end
   end
-
-  def index
-    @books = Book.all
-  end
-
+  
   def show
       @book = Book.find(params[:id])
   end
