049853a3d9dde7b5009f58656fdd571d5d291771
fmy1995 <fmy.kymz444@gmail.com>
2020-12-26 08:59:57 +0000

    12/26

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -28,6 +28,7 @@ end
   validates :address_street, presence: true
 
 
+
   has_many :follower_relationships,foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :follower_relationships
   has_many :followed_relationships,foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
